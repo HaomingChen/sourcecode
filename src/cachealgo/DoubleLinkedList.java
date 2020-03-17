8e9d0f65aed41302358527b140af05f52775d2ee
HaomingChen <haomingc1993@gmail.com>
2020-03-17 18:34:33 +0800

    1. 高速缓存FIFO算法实现

--- src/cachealgo/DoubleLinkedList.java
+++ src/cachealgo/DoubleLinkedList.java
@@ -14,6 +14,8 @@ public class DoubleLinkedList {
         this.capacity = capacity;
         head = new Node();
         tail = new Node();
+        head.next = tail;
+        tail.prev = head;
         size = 0;
     }
 
@@ -39,16 +41,58 @@ public class DoubleLinkedList {
         size++;
     }
 
-    public void remove(Node node) {
+    public int delHead() {
         if (size <= 0) {
-            return;
+            return -1;
+        }
+        return remove(head.next);
+    }
+
+    public int delTail() {
+        if (size <= 0) {
+            return -1;
+        }
+        return remove(tail.prev);
+    }
+
+    public int remove(Node node) {
+        if (size <= 0) {
+            return -1;
         }
         node.prev.next = node.next;
         node.next.prev = node.prev;
         node.next = null;
         node.prev = null;
         size--;
+        return node.val;
     }
 
+    //弹出头部节点
+    public int pop() {
+        return delHead();
+    }
+
+    //添加节点
+    public int append(Node node) {
+        addLast(node);
+        return node.val;
+    }
+
+    //头部添加节点
+    public int appendFront(Node node) {
+        return append(node);
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("[");
+        Node pt = head.next;
+        while (pt != null) {
+            sb.append(pt.val + ", ");
+            pt = pt.next;
+        }
+        return sb.toString().substring(0, sb.length() - 1) + "]";
+    }
 }
 
--- src/cachealgo/FIFO.java
+++ src/cachealgo/FIFO.java
@@ -1,4 +1,79 @@
 package cachealgo;
 
+import java.util.HashMap;
+
 public class FIFO {
+
+    class Node {
+
+        int key;
+        int value;
+        Node next;
+        Node prev;
+
+        public Node(int key, int value) {
+            this.key = key;
+            this.value = value;
+        }
+
+        public Node() {
+
+        }
+
+    }
+
+    HashMap<Integer, Node> dic;
+    int size;
+    Node head;
+    Node tail;
+    int capacity;
+
+    public FIFO(int capacity) {
+        dic = new HashMap<>(capacity);
+        head = new Node();
+        tail = new Node();
+        head.next = tail;
+        tail.prev = head;
+        this.capacity = capacity;
+        size = 0;
+    }
+
+    public void put(int key, int value) {
+        if (capacity >= size) {
+            remove(head.next.key);
+        }
+        insert(key, value);
+    }
+
+    public int get(int key) {
+        if (!dic.containsKey(key)) {
+            return -1;
+        }
+        return dic.get(key).value;
+    }
+
+    private void remove(int key) {
+        if (size <= 0 || !dic.containsKey(key)) {
+            return;
+        }
+        Node node = dic.get(key);
+        node.prev.next = node.next;
+        node.next.prev = node.prev;
+        size--;
+    }
+
+    private void insert(int key, int value) {
+        if (dic.containsKey(key)) {
+            Node node = dic.get(key);
+            node.value = value;
+            return;
+        }
+        Node node = new Node(key, value);
+        node.prev = head;
+        node.next = head.next;
+        head.next.prev = node;
+        head.next = node;
+        size++;
+    }
+
 }
